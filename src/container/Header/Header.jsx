9ce3aa81e62c563342473971d8c5372a2640dbbc
Viola8905 <viola89055@gmail.com>
2022-03-23 16:44:10 +0200

    did Header section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
-
+import { SubHeading } from '../../components';
+import {images} from "../../constants"
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+  <div className="app__header app__wrapper section__padding" id='home'>
+		<div className="app__wrapper_info">
+				<SubHeading title='Chase the new flavour'/>
+				<h1 className='app__header-h1'>The key to Fine Dining</h1>
+				<p className="p__opensans" style={{margin:'2rem 0'}}>
+						Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam cupiditate rerum recusandae quas quis ab odio, incidunt, dicta modi tempora soluta, nulla saepe debitis voluptate.
+				</p>
+				<button className="custom__button">
+					Explore Menu
+				</button>
+		</div>
+		<div className="app__wrapper_img">
+				<img src={images.welcome} alt="header img" />
+		</div>
+	</div>
 );
 
 export default Header;
