d132a41031dee9ac235f77f34820fbea93f2ec7e
Viola <viola89055@gmail.com>
2023-05-30 17:41:55 +0200

    feat: add book table component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,50 @@
-import React from 'react';
+import React from "react";
 
-import { AboutUs, Chef, FindUs, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
-import { Navbar } from './components';
-import './App.css';
+import {
+  AboutUs,
+  Chef,
+  FindUs,
+  Footer,
+  Gallery,
+  Header,
+  Intro,
+  Laurels,
+  SpecialMenu,
+} from "./container";
+import { Navbar } from "./components";
+import "./App.css";
 
 const App = () => (
-  <div className='wrapper'>
+  <div className="wrapper">
     <Navbar />
     <Header />
-    <AboutUs />
+    {/* <AboutUs /> */}
     <SpecialMenu />
     <Chef />
     <Intro />
     <Laurels />
-    <Gallery />
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        backgroundColor: `var(--color-black)`,
+				padding:"30px 10px"
+      }}
+    >
+      <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSd_tZb0l7xXM_xhyQ-BU9sB7T1ZkeRB2Q-A47MGCTYXpA170Q/viewform?embedded=true"
+        width="640"
+        height="991"
+        frameborder="0"
+        marginheight="0"
+        marginwidth="0"
+      >
+        Загрузка…
+      </iframe>
+    </div>
+    {/* <Gallery />
     <FindUs />
-    <Footer />
+    <Footer /> */}
   </div>
 );
 
